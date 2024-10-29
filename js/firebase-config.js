a3cef3146ee2934d6e96edfa1a88403de2c3dc33
Kevinb59 <130929760+Kevinb59@users.noreply.github.com>
2024-10-29 02:53:37 +0100

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDbI0ONVdZidBQRZRRkyQAoPOXarQ9vWXY",
+  authDomain: "e-commerce-79a3c.firebaseapp.com",
+  projectId: "e-commerce-79a3c",
+  storageBucket: "e-commerce-79a3c.appspot.com",
+  messagingSenderId: "574561818402",
+  appId: "1:574561818402:web:c2ace53715b70cb4e1777b",
+  measurementId: "G-GQK0S753TQ"
+};
